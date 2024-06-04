dc128661af4f70531266485ef5c7fbbb865bb1de
sabilafiridas <sabilafirdias@gmail.com>
2024-06-04 11:10:39 +0700

    membuat blok catch

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -14,5 +14,12 @@ int main()
         //cout<<data.at(5)<<endl;
         //memanggil array elemen ke-5
     }
-    
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
