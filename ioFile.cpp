df9b0fcb11a5717b00102b39427ca65df3087041
sabilafiridas <sabilafirdias@gmail.com>
2024-06-04 10:48:51 +0700

    membuka file dalam mode membaca

--- ioFile.cpp
+++ ioFile.cpp
@@ -28,6 +28,24 @@ int main()
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
     
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open()) {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)) {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
 
