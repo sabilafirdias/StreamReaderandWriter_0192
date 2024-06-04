1adef289aa4abaac6f900943f345a094c464a1f9
sabilafiridas <sabilafirdias@gmail.com>
2024-06-04 10:29:20 +0700

    membuka file

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,6 +8,11 @@ int main()
     //untuk menyimpan data berbentuk string
     string baris;
 
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
     
 }
 
